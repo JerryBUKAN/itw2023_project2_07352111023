b8659bb401a3b97defaaf6ebc73439cfa01fb0c6
JerryBUKAN <jerryantopareda2003@gmail.com>
2024-01-07 05:42:20 +0700

    U

---
+++ js/myScript.js
@@ -0,0 +1,10 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     const images = document.querySelectorAll('.zoom-img');
+  
+//     images.forEach(function(image) {
+//       image.addEventListener('click', function() {
+//         this.classList.toggle('img-zoom');
+//       });
+//     });
+//   });
+  
